ca5dd0d54ebfec808c53ae850749f8eb5fd78f9d
Sunjoo IM <tjss1580@gmail.com>
2023-07-15 14:48:07 +0900

    Add: project 02

---
+++ 02_GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sunjoo IM on 2023/07/15.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
